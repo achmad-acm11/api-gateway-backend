676465c56883711a215caeeda2c51f88f61a680f
achmad-acm11 <72180566+achmad-acm11@users.noreply.github.com>
2022-01-23 08:50:29 +0700

    fix refresh token endpoint

--- routes/handler/refresh_tokens/create.js
+++ routes/handler/refresh_tokens/create.js
@@ -67,12 +67,6 @@ module.exports = {
           },
         });
       });
-
-      const response = await axios.post(
-        `${URL_SERVICE_USER}/refresh-token`,
-        req.body
-      );
-      return res.json(response.data);
     } catch (error) {
       const { status, data } = error.response;
       return res.status(status).json(data);
